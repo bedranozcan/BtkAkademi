c9db052a534963b64a1ffeb3c0982bb65cb9d813
bedranozcan <bedra@IDVLABS>
2023-09-14 22:13:21 +0300

    In-Memory'de dataları tutma ve Feedback sayfası

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using BtkAkademi.Models;
+namespace BtkAkademi.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+        public IActionResult Apply()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Apply([FromForm]Candidate model)
+        {
+            Repository.Add(model);
+            return View("Feedback",model);
+        }
+    }
+}
